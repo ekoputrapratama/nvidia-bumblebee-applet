ab658bd593cb547dc1b04844d733c444a223506c
muhammadsayuti <muhammad.sayuti94@gmail.com>
2018-02-18 00:29:21 +0800

    add script to make executable file

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 pkgname=nvidia-bumblebee-applet
 srcdir=$(pwd)
+
+echo "building executable file"
+valac --pkg gtk+-3.0 --pkg glib-2.0 nvidia-bumblebee.vala
+
 cd /usr/lib
 if [ -d $pkgname ] 
 then
@@ -16,6 +20,7 @@ rm -rf .git
 echo "Removing dev files"
 rm -f PKGBUILD
 rm -f README.md
+rm -f nvidia-bumblebee.vala
 
 cp -dpr --no-preserve=ownership ./icons/*.svg /usr/share/icons/
 cp -dpr --no-preserve=ownership ./icons/nvidia-bumblebee.png /usr/share/icons/
